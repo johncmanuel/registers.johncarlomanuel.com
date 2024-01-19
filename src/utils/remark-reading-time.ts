f065af6b321a5d07834fc0bc00055b790698deff
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-01-19 12:43:22 -0800

    update type

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -2,7 +2,8 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
 export function remarkReadingTime() {
-	return function (tree, { data }) {
+	// TODO: Perhaps add a type for these params?
+	return function (tree: any, { data }: any) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
 		data.astro.frontmatter.minutesRead = readingTime.text;
